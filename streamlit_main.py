801889a199ddff01412138532ed8e22d82177bd5
Vanya88 <vanyachen88@gmail.com>
2026-09-03 09:42:32 +0800

    fix

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
+pgnav = st.navigation(
+    pages= [ "pages/home.py" , "pages/mycv.py" , "pages/myproject.py" ],   
+    position="top"
+) 
 
-pgnav = st.navigation (
-      pages= ［"pages/home.py", "pages/mycv.py" , "pages/contactme.py","pages/myproject.py"］,
-      position = "top"
-)
 pgnav.run()
