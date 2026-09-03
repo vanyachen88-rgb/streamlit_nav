6603870933dbe7e2c5ba28d875cd267cad73a2ae
Vanya88 <vanyachen88@gmail.com>
2026-09-03 10:30:28 +0800

    menuList

--- pages/myproject.py
+++ pages/myproject.py
@@ -1,1 +1,9 @@
 import streamlit as st
+st.title("專案首頁")
+
+
+with st.sidebar:
+    st.write("專案清單")
+    st.write("**專案說明")
+    st.write("**專案功能")
+    st.write("**專案使用工具")
---
+++ streamlit_main3.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+
+menuList= {
+     "功能A": [
+                st.Page("pages/home.py" , title="Home",icon=":material/home:" ),
+                st.Page("pages/mycv.py" , title="我的履歷" ),
+                st.Page("https://github.com/mymis168", title="我的GitHub")                   
+            ],
+      "專案列表": [
+                st.Page("pages/project1.py" , title="執行專案"),
+                st.Page("pages/project2.py" , title="執行專案2"),
+                st.Page("pages/myproject.py" , title="執行專案3"),
+                st.Page("https://github.com/mymis168/streamlit_nav", title="nav專案"    
+                  ]
+}
+
+pageList=[
+    st.Page("pages/home.py" , title="Home" ),
+    st.Page("pages/mycv.py" , title="我的履歷" ),   
+    st.Page("pages/myproject.py" , title="執行專案3")    
+]
+# st.navigation 建立後回傳一個 pages 物件 , 並不會直接執行
+pgnav = st.navigation(
+    pages=menuList,
+    position="top"
+)
+
+# 呼叫 pages 物件的 run() 方法 , 會依照使用者選擇的頁面 , 執行對應的程式碼
+pgnav.run()
